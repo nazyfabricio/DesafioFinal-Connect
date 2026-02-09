bac731044861a97e5d0988558d5dd17baec31668
nazyfabricio <nazymfabricio@gmail.com>
2026-02-09 13:45:27 +0000

    Estilizando a rota Ações da Connect e desenvolvendo rota Quem somos

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,19 +1,51 @@
 import React from "react";
 import styles from "./Perfil.module.scss";
+import FotoPerfil from "../../assets/perfil.jpg";
 
 function Perfil() {
   return (
     <>
       <section className={styles.perfil}>
-        <h2>Nazy Fabricio</h2>
-        <h5>
+
+        <article>
+     <img src={FotoPerfil} alt="" />
+
+     <div>
+<h2>Nazy Fabricio</h2>
+  <h3>Voluntário ativo</h3>
+
+
+
+        <p className={styles.paragrafoSobreMim}>
           Sou uma aspirante na tecnologia, nunca me interessei muito nessa área,
           mas de 2023 pra cá começou a ser minha paixão e graças ao curso da Vai
           Na Web pude apreder muito. Muito Obriagda à Vai Na Web{" "}
-        </h5>
-        <p>Recife, PE</p>
+        </p>
+
+<address>
+<p>Recife, PE</p>
         <p>nazym931@gmail.com</p>
         <p>Membro desde 2025</p>
+
+</address>
+
+<ul className={styles.AreasDeInteresse}>
+ 
+          <li>Educação</li>
+          <li>Meio Ambiente</li>
+          <li>Assistência Social</li>
+          <li>Design</li>
+        
+
+</ul>
+     </div>
+
+        </article>
+   
+        
+      
+        
+        >
       </section>
     </>
   );
--- src/components/QuemSomos/QuemSomos.jsx
+++ src/components/QuemSomos/QuemSomos.jsx
@@ -1,27 +1,33 @@
 import React from "react";
-import styles from './QuemSomos.module.scss'
+import styles from "./QuemSomos.module.scss";
 
 function QuemSomos() {
   return (
-    <>
     <section className={styles.quemSomos}>
-      <h4>Como a Connect atua em projetos sociais</h4>
-      <p>
-        A Connect é uma iniciativa que conecta empresas, pessoas e organizações
-        sociais com um propósito em comum: gerar impacto positivo e transformar
-        realidades. Atuamos no desenvolvimento e apoio a projetos sociais nas
-        áreas de educação, inclusão, tecnologia, esporte e assistência social,
-        sempre com foco em impacto real e sustentável. Há anos, trabalhamos
-        aproximando quem quer ajudar de quem precisa, promovendo ações sociais
-        estruturadas, transparentes e alinhadas às necessidades das comunidades.
-        Acreditamos que a transformação acontece quando propósito, colaboração e
-        responsabilidade caminham juntos. Nosso compromisso é conectar boas
-        causas a quem deseja fazer a diferença, criando pontes entre empresas,
-        voluntários e projetos sociais que realmente transformam vidas.
-      </p>
-</section>
+      <div className={styles.boxTextoQuemSomos}>
+        <h2>Como a Connect atua em projetos sociais</h2>
+        <p>
+          A Connect é uma iniciativa que conecta empresas, pessoas e
+          organizações sociais com um propósito em comum: gerar impacto positivo
+          e transformar realidades. Atuamos no desenvolvimento e apoio a
+          projetos sociais nas áreas de educação, inclusão, tecnologia, esporte
+          e assistência social, sempre com foco em impacto real e sustentável.
+        </p>
 
-    </>
+        <p>
+          Há anos, trabalhamos aproximando quem quer ajudar de quem precisa,
+          promovendo ações sociais estruturadas, transparentes e alinhadas às
+          necessidades das comunidades. Acreditamos que a transformação acontece
+          quando propósito, colaboração e responsabilidade caminham juntos.
+        </p>
+
+        <p>
+          Nosso compromisso é conectar boas causas a quem deseja fazer a
+          diferença, criando pontes entre empresas, voluntários e projetos
+          sociais que realmente transformam vidas.
+        </p>
+      </div>
+    </section>
   );
 }
 export default QuemSomos;
